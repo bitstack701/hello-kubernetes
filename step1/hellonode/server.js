697900306beef8d4124a5696394c55d77ade18b5
bitstack <bitstack701@gmail.com>
2017-08-09 16:23:30 +0200

    Added a log message when the server starts up.

--- step1/hellonode/server.js
+++ step1/hellonode/server.js
@@ -3,8 +3,10 @@ const http = require('http');
 const handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World Again!!!');
+  response.end('Hello World!!!');
 };
 
 const www = http.createServer(handleRequest);
-www.listen(8080);
+www.listen(8080, function() {
+  console.log("Server listening on port 8080");
+});
